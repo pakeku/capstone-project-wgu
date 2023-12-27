e03b5b31ada88b221fc1332437332e53c3e46d56
pakeku <elina13@wgu.edu>
2023-12-27 14:41:37 -0500

    Implement middlewares and pass tests

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express, json } from "express";
+import express, { Express } from "express";
 import { loggerMiddleware } from "@middlewares/logger";
 import { notFoundRouter } from '@routes/error'
 import { healthRouter } from '@routes/health'
@@ -6,18 +6,20 @@ import { compressionMiddleware } from '@middlewares/compression';
 import { rateLimitMiddleware } from '@middlewares/rate-limit';
 import { errorMiddleware } from '@middlewares/error';
 import { helmetMiddleware } from '@middlewares/helmet';
-import { corsMiddleware } from "@middlewares/cors";
+import { jsonMiddleware } from "@middlewares/json";
+import { corsMiddleware } from '@middlewares/cors';
 
 // Initialize Express app
 const app: Express = express();
 
 // Middlewares
-app.use(json());                   // Parse incoming JSON requests
+app.use(jsonMiddleware);           // Parse incoming JSON requests
 app.use(loggerMiddleware);         // Log incoming requests
 app.use(helmetMiddleware());       // Set HTTP headers for security
 app.use(compressionMiddleware());  // Compress HTTP responses
 app.use(rateLimitMiddleware());    // Apply rate limiting to requests
 app.use(errorMiddleware);          // Handle errors
+app.use(corsMiddleware)            // Enable CORS
 
 // Routes
 app.use(healthRouter);              // Health-related routes
--- src/middlewares/cors/index.ts
+++ src/middlewares/cors/index.ts
@@ -1,21 +1,24 @@
-import cors from 'cors';
-import { envVariableCheck } from '@utils/checkEnvVariables';
+import { Request, Response, NextFunction } from 'express';
+import cors, { CorsOptions } from 'cors';
 
-const requiredEnvVariables: string[] = ['CORS_ALLOWED_ORIGINS'];
+const whitelist: string[] = (process.env.CORS_ALLOWED_ORIGINS || '').split(',');
 
-export const corsMiddleware = async () => {
-    await envVariableCheck(requiredEnvVariables, () => {
-        const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS?.split(',') || [];
+const corsOptionsDelegate = (req: Request, callback: (err: Error | null, options?: CorsOptions) => void) => {
+    let corsOptions: CorsOptions;
+    if (whitelist.indexOf(req.header('Origin') || '') !== -1) {
+        corsOptions = { origin: true };
+    } else {
+        corsOptions = { origin: false };
+    }
+    callback(null, corsOptions);
+};
 
-        // Return the actual cors middleware function
-        return cors({
-            origin: function (origin, callback) {
-                if (!origin || allowedOrigins.includes(origin)) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not allowed by CORS'));
-                }
-            },
-        });
+// Define CORS middleware
+export const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    cors(corsOptionsDelegate)(req, res, (err) => {
+        if (err) {
+            return next(err);
+        }
+        next();
     });
 };
