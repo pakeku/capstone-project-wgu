598b78304f768a764e11cd06b7534c5f61b53373
pakeku <elina13@wgu.edu>
2023-12-27 14:48:27 -0500

    Comment for better developer informatino

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import { loggerMiddleware } from "@middlewares/logger";
-import { notFoundRouter } from '@routes/error'
-import { healthRouter } from '@routes/health'
+import { notFoundRouter } from '@routes/error';
+import { healthRouter } from '@routes/health';
 import { compressionMiddleware } from '@middlewares/compression';
 import { rateLimitMiddleware } from '@middlewares/rate-limit';
 import { errorMiddleware } from '@middlewares/error';
@@ -12,20 +12,34 @@ import { corsMiddleware } from '@middlewares/cors';
 // Initialize Express app
 const app: Express = express();
 
-// Middlewares
-app.use(jsonMiddleware);           // Parse incoming JSON requests
-app.use(loggerMiddleware);         // Log incoming requests
-app.use(helmetMiddleware());       // Set HTTP headers for security
-app.use(compressionMiddleware());  // Compress HTTP responses
-app.use(rateLimitMiddleware());    // Apply rate limiting to requests
-app.use(errorMiddleware);          // Handle errors
-app.use(corsMiddleware)            // Enable CORS
+/** Middlewares */
+// Parse incoming JSON requests
+app.use(jsonMiddleware);
 
-// Routes
-app.use(healthRouter);              // Health-related routes
+// Log incoming requests
+app.use(loggerMiddleware);         
+
+// Set HTTP headers for security
+app.use(helmetMiddleware());       
+
+// Compress HTTP responses
+app.use(compressionMiddleware());  
+
+// Apply rate limiting to requests
+app.use(rateLimitMiddleware());    
+
+// Handle errors
+app.use(errorMiddleware);          
+
+// Enable CORS
+app.use(corsMiddleware);            
+
+/** Routes */
+// Health-related routes
+app.use(healthRouter);
 
 /*
-Ensure that the route for /health is defined before the 404 route. 
+Ensure that the 404 route is applied last. 
 Routes are matched in the order they are defined, so if the 404 route 
 is defined first, it might capture other routes as an unknown route.
 */
